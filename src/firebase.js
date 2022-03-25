226d3535f88f22a54e436278a3df2caa6896b9b5
rathi710 <vr382003@gmail.com>
2022-03-25 20:13:26 +0530

    commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtySwK7bCERK5TM9crDk98nLy7le-PENE",
+  authDomain: "netflix-clone-7aaaf.firebaseapp.com",
+  projectId: "netflix-clone-7aaaf",
+  storageBucket: "netflix-clone-7aaaf.appspot.com",
+  messagingSenderId: "886967025523",
+  appId: "1:886967025523:web:93e5a9bef2ec3d5c6d8f06",
+};
