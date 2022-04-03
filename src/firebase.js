435748d9c64db7d8b61ab74baadef0706e356c03
rathi310 <vishesh.rathi.710@gmail.com>
2022-04-04 03:37:22 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const config = {
-  apiKey: "AIzaSyBtySwK7bCERK5TM9crDk98nLy7le-PENE",
-  authDomain: "netflix-clone-7aaaf.firebaseapp.com",
-  projectId: "netflix-clone-7aaaf",
-  storageBucket: "netflix-clone-7aaaf.appspot.com",
-  messagingSenderId: "886967025523",
-  appId: "1:886967025523:web:93e5a9bef2ec3d5c6d8f06",
+  apiKey: "AIzaSyDATGXtZ0OGivzfRRbv7NHw_cbJmPnHzQM",
+  authDomain: "netflix-clone-d3267.firebaseapp.com",
+  projectId: "netflix-clone-d3267",
+  storageBucket: "netflix-clone-d3267.appspot.com",
+  messagingSenderId: "249619634219",
+  appId: "1:249619634219:web:e9832af3ec1ccf68d3b26e"
 };
 
 const firebaseApp = firebase.initializeApp(config);
